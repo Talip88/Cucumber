5357f2ff16a342328324f0a5825e36a15ab7b707
COLAK <110104775+Talip88@users.noreply.github.com>
2023-01-18 04:14:13 +0300

    eklemeler yapıldı

--- src/test/java/Runners/_06_TestRunnerSmokeWithPlugin.java
+++ src/test/java/Runners/_06_TestRunnerSmokeWithPlugin.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinitions"},
-        plugin= {"html:target/site/cucumber-pretty.html"}
+        //plugin= {"html:target/site/cucumber-pretty.html"}
+        plugin= {"pretty","html:target/site/cucumber-pretty","json:target/cucumber/cucumber.json"} // JENKINS ICIN PLUG IN
 )
 public class _06_TestRunnerSmokeWithPlugin extends AbstractTestNGCucumberTests {
 }
